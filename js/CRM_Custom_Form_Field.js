7579147744e39fe08e76088d46f24797e9105ee6
Allen Shaw <allen@JoineryHQ.com>
2021-06-04 12:32:15 -0500

    Removed redundant `$(document).ready()` function wrapper.

--- js/CRM_Custom_Form_Field.js
+++ js/CRM_Custom_Form_Field.js
@@ -1,37 +1,35 @@
 CRM.$(function($) {
-  $(document).ready(function(){
-    // Add limit_per field to the right place and remove original field
-    var limitPer = $('#limit_per');
-    var limitPerAmount = $('#limit_per_amount');
-    var limitPerParent = limitPer.parents('tr');
-    var limitPerAmountParent = limitPerAmount.parents('tr');
-    limitPerParent.addClass('crm-custom-field-form-block-min_value limit_per').hide();
-    limitPerAmountParent.addClass('crm-custom-field-form-block-min_value limit_per_amount').hide();
-    limitPerParent.insertAfter('.crm-custom-field-form-block-is_view');
-    limitPerAmountParent.insertAfter('.limit_per');
+  // Add limit_per field to the right place and remove original field
+  var limitPer = $('#limit_per');
+  var limitPerAmount = $('#limit_per_amount');
+  var limitPerParent = limitPer.parents('tr');
+  var limitPerAmountParent = limitPerAmount.parents('tr');
+  limitPerParent.addClass('crm-custom-field-form-block-min_value limit_per').hide();
+  limitPerAmountParent.addClass('crm-custom-field-form-block-min_value limit_per_amount').hide();
+  limitPerParent.insertAfter('.crm-custom-field-form-block-is_view');
+  limitPerAmountParent.insertAfter('.limit_per');
 
-    if(limitPer.val() >= 1) {
-      limitPerAmountParent.show();
-    }
+  if(limitPer.val() >= 1) {
+    limitPerAmountParent.show();
+  }
 
+  if($('#html_type').val() === 'Select') {
+    limitPerParent.show();
+  }
+
+  $(document).on('change', '#html_type', function(){
     if($('#html_type').val() === 'Select') {
       limitPerParent.show();
+    } else {
+      limitPerParent.hide();
     }
+  });
 
-    $(document).on('change', '#html_type', function(){
-      if($('#html_type').val() === 'Select') {
-        limitPerParent.show();
-      } else {
-        limitPerParent.hide();
-      }
-    });
-
-    limitPer.on('change', function(){
-      if($(this).val() >= 1) {
-        limitPerAmountParent.show();
-      } else {
-        limitPerAmountParent.hide();
-      }
-    });
+  limitPer.on('change', function(){
+    if($(this).val() >= 1) {
+      limitPerAmountParent.show();
+    } else {
+      limitPerAmountParent.hide();
+    }
   });
 });
